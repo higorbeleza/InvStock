a0fb6cb18169bb7179dc0036a6165074ee2a74b9
higorbeleza <higorbeleza49@gmail.com>
2023-08-24 15:21:10 -0400

    feat(higorbeleza): model product

---
+++ src/models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    id: {type: String, require},
+    name: {type: String, require},
+    description: {type: String, require},
+    price: {type: String, require},
+    amount: {type: Number, require}
+});
+
+const Products = mongoose.model('Products', productSchema);
+
+module.exports = Products;
