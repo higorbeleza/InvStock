622c48ff7dd6c0fc81453d8fe3d135ead4cac33e
higorbeleza <higorbeleza49@gmail.com>
2023-08-24 15:08:11 -0400

    feat(higorbeleza): Connect to database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connect to database');
+}).catch((error) => {
+    console.log('MongoDB connection error:', error);
+});
+
+app.use(express.json());
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
-})
+});
