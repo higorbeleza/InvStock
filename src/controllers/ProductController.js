1977c4ab9aaec7c4442beedc9fa68f6431e3a4ad
higorbeleza <higorbeleza49@gmail.com>
2023-08-25 00:45:14 -0400

    feat(higorbeleza): read, find & update

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -16,11 +16,45 @@ class ProductController {
         }
     }
 
+    static findAll = async (req, res) => {
+        try {
+            const products = await Products.findAll();
+            return res.status(200).json(products);
+        } catch (error) {
+            return res.status(500).send({message: error.message});
+        }
+    }
+
+    static findById = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const product = await Products.findById(id);
+            
+            if (product !== null) {
+                return res.status(200).json(product);
+            } else {
+                return res.status(500).send({message: "Id not found"});
+            }
+        } catch (error) {
+            return res.status(500).send({message: "Internal server error"});
+        }
+    }
+
     static findByName = async (req, res) => {
         const name = req.body.name;
         try {
             const product = await Products.find({ name: name });
             return res.status(200).json(product);
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
+    }
+
+    static updateProduct = async (req, res) => {
+        const id = req.body.id;
+        try {
+            const product = await Products.findByIdAndUpdate(id);
+            return res.status().json({message: 'Produto atualizado: ', product});
         } catch (error) {
             return res.status(400).json({message: error.message});
         }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/add_product', ProductController.add_product);
 router.get('/product_name', ProductController.findByName);
+router.get('/all_products', ProductController.findAll);
+router.get('/product_id/:id', ProductController.findById);
 
 module.exports = router;
